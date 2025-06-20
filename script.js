f22bf5e42a6c86a46699a9f670d4728b6ce4d96f
Aquaday <martin991940@hotmail.com>
2025-06-20 13:32:30 +0200

    building map

--- script.js
+++ script.js
@@ -14,17 +14,9 @@ canvas.width = tilesWidth * rowTiles;
 
 context = canvas.getContext('2d');
 
-let colonyImage = new Image()
-colonyImage.src = "./images/colony-db32.png"
-
 let floorTilesImage = new Image()
 floorTilesImage.src = "./images/terrain.png"
-
 let floorArray = []
-
-// 417 x 328
-
-//drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
 
 floorTilesImage.onload = () => {
     createFloor()
@@ -32,17 +24,12 @@ floorTilesImage.onload = () => {
 }
 
 function update() {
-    // context.fillStyle = 'black';
-    // context.fillRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < floorArray.length; i++) {
         floorArray[i].draw();
       }
 }
 
-
-
 function createFloor() {
-
     for (let i = 0; i < columnTiles; i++) {
         let randomNumber = Math.floor(Math.random() * 3) + 15
         for (let j = 0; j < rowTiles; j++) {
@@ -51,29 +38,22 @@ function createFloor() {
         } else {
             floorArray.push(new grassFloor(tilesWidth * j, tilesHeight * i))
         }
-            console.log(randomNumber)
         }
     }   
-
 }
-
-//drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
 
 class grassFloor {
     constructor(dX, dY) {
         this.sX = tilesSize * 0
         this.sY = tilesSize * 5
-        this.sWidth = tilesWidth
-        this.sHeight = tilesHeight
         this.dX = dX
         this.dY = dY
-    
     }
 
     draw() {
         context.drawImage(
             floorTilesImage,
-            this.sX, this.sY, this.sWidth, this.sHeight,
+            this.sX, this.sY, tilesWidth, tilesHeight,
             this.dX, this.dY, tilesWidth, tilesHeight
         );
     }
@@ -83,17 +63,14 @@ class waterFloor {
     constructor(dX, dY) {
         this.sX = tilesSize * 29
         this.sY = tilesSize * 5
-        this.sWidth = tilesWidth
-        this.sHeight = tilesHeight
         this.dX = dX
         this.dY = dY
-    
     }
 
     draw() {
         context.drawImage(
             floorTilesImage,
-            this.sX, this.sY, this.sWidth, this.sHeight,
+            this.sX, this.sY, tilesWidth, tilesHeight,
             this.dX, this.dY, tilesWidth, tilesHeight
         );
     }
